4fd2bc9d478a4382a2d67ff2a42fa1c624827db1
Glen112 <escobar.glen@gmail.com>
2022-06-25 04:59:37 -0300

    Dio-project-react-styed-components

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,9 +1,34 @@
+import styled from 'styled-components';
+import {string} from 'prop-types';
+
 export const Quotes = ({quote, speaker}) =>{
   return(
-    <div>
-      <p>{quote}</p>
-      <p>- {speaker}</p>
+    <Wrapper>
+      <Quote>{quote}</Quote>
+      <Speaker>- {speaker}</Speaker>
       <button>click</button>
-    </div>
-  )
-}
+    </Wrapper>
+  );
+};
+
+Quotes.protoTypes = {
+  quote: string,
+  speaker: string
+};
+
+const Wrapper = styled.div`
+flex:1;
+display:flex;
+flex-direction:column;
+align-items: center; 
+`
+const Quote = styled.p`
+  font-size: 2em;
+  text-shadow: rgba(0, 0, 0, 0.2) 1px 1px 1px;
+  flex: 1;
+  margin: 0;
+`;
+const Speaker = styled(Quote)`
+text-align:right;
+margin-bottom:50px;
+`
