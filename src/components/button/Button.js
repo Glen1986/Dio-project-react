6c4c23fb536abbfcd01f8931207025165d776ef1
Glen112 <escobar.glen@gmail.com>
2022-06-25 16:34:09 -0300

    Dio-project-react-styed-components-test

---
+++ src/components/button/Button.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  background:#dc872c;
+  color: black;
+  border: none;
+  font-size:1.5rem;
+  font-family: 'New Tegomin', serif;
+  padding: 10px 20px;
+  cursor:pointer;
+  box-shadow:#333 6px 6px;
+  border-radius:8%;
+
+  &:hover{
+  background:#a40000  
+}
+`
--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,19 +1,21 @@
 import styled from 'styled-components';
-import {string} from 'prop-types';
+import {string, func} from 'prop-types';
+import {Button} from '../button';
 
-export const Quotes = ({quote, speaker}) =>{
+export const Quotes = ({quote, speaker, onUpdate}) =>{
   return(
     <Wrapper>
       <Quote>{quote}</Quote>
       <Speaker>- {speaker}</Speaker>
-      <button>click</button>
+      <Button onClick={onUpdate}>click here</Button>
     </Wrapper>
   );
 };
 
 Quotes.protoTypes = {
   quote: string,
-  speaker: string
+  speaker: string,
+  onUpdate:func
 };
 
 const Wrapper = styled.div`
